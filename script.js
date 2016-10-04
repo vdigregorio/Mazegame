3eb70a23265b2fface03f28c0d28d2968f0046e2
Valeria Di Gregorio <clear>
2016-10-04 10:52:18 -0400

    pseudocoding and html css created

---
+++ script.js
@@ -0,0 +1,16 @@
+console.log('IM WORKING!')
+
+//first i have to create a div to move through the maze and call it 'pawn'
+
+//create keydown functions for arrows to control said div 'pawn'
+
+//create collision detection with something called getImageData()
+//that apparently only works with servers! so check that get form out!
+
+//add animation for end of maze! winner pop up an disintegration of div!
+
+//add event listeners at end! so like window.addEventListener('keydown',doKeyDown,true);
+
+
+//figure out theme for maze!
+
