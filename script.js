5835bda6164122b8399dc2d960db127e494ba3aa
Valeria Di Gregorio <clear>
2016-10-04 13:41:10 -0400

    started writing out functions

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 console.log('IM WORKING!')
 
-//make div replicate #stacker-rep ontop of eachother once spacebar is hit
+//make div replicate #stacker ontop of eachother once spacebar is hit
 //if ontop of stack, you continue, otherwise the game is over
+ $(document).ready(function(){
 
+function replicate(){
+
+}
+
+function stackEm(){
+
+}
+
+//for loop that switches class every time you press spacebars
+
+
+
+ });
 // make div move side to side using maybe setinterval()
 
 
