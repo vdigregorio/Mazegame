7d0dfc0d48ed9780d3075f2814667b2edde89abe
Valeria Di Gregorio <clear>
2016-10-04 12:20:43 -0400

    commiting pseudocode

--- script.js
+++ script.js
@@ -1,16 +1,11 @@
 console.log('IM WORKING!')
 
-//first i have to create a div to move through the maze and call it 'pawn'
+//make div replicate #stacker-rep ontop of eachother once spacebar is hit
+//if ontop of stack, you continue, otherwise the game is over
 
-//create keydown functions for arrows to control said div 'pawn'
-
-//create collision detection with something called getImageData()
-//that apparently only works with servers! so check that get form out!
-
-//add animation for end of maze! winner pop up an disintegration of div!
-
-//add event listeners at end! so like window.addEventListener('keydown',doKeyDown,true);
+// make div move side to side using maybe setinterval()
 
 
-//figure out theme for maze!
+//randomize the color, so every stack is different **SELF BONUS
+
 
