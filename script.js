3608d89a0d8fe1e0c312140fd7ec95d6d841aaa7
Valeria Di Gregorio <clear>
2016-10-10 19:54:41 -0400

    more updates

--- script.js
+++ script.js
@@ -3,64 +3,18 @@ console.log('IM WORKING!')
 
 $(document).ready(function() {
 
- $('.colors').on('click', function(){
-      $('.stacker .tester2 .tester4 .tester6 .tester8 .tester10').css('background-color', 'blue')
-      $('.tester1 .tester3 .tester5 .tester7 .tester9 .tester11').css('background-color', 'purple')
+    $('.colors').on('click', function() {
+        $('.stacker .tester2 .tester4 .tester6 .tester8 .tester10').css('background-color', 'blue')
+        $('.tester1 .tester3 .tester5 .tester7 .tester9 .tester11').css('background-color', 'purple')
     });
-    // var searchUrl = window.location.search.charAt(1);
-    //    var split = [];
-    //    split = searchUrl.split("&");
-    //    // var obj = {};
-    //    var form = $('.list-maker-form');
 
-    //    for (i = 0; i < split.length; i++) {
-    //        var arr = [];
-    //        arr = split[i].split("=");
-    //        var key = $('.one');
-
-
-    //        var p1 = $('<p>');
-
-    //        p1.text(key);
-
-    //        $('.box').append(p1);
-
-
-    //        var input = $('.box');
-    //        input.type = 'hidden';
-    //        // input = key;
-    //        form.appendTo(input);
-    //    };
 
 
     var moving = true;
     var speed = 10;
-    var targetBlock;
+    // var targetBlock;
 
 
-    // var going = window.setInterval(function() {
-    //     // the count should be reset to 0 when it reaches 100
-    //     var count = $('.stacker').data('count');
-
-    //     // if the count reaches 100 set to 0
-    //     if (count === 400) {
-    //         $('.stacker').toggleClass('right');
-    //         $('.stacker').data('count', 0);
-    //     } else {
-    //         $('.stacker').data('count', count + 1);
-    //     }
-
-    //     var position = $(".stacker").position();
-
-    //     if ($('.stacker').hasClass('right')) {
-    //         $(".stacker").css({ left: position.left + 1 });
-    //     } else {
-    //         $(".stacker").css({ left: position.left - 1 });
-    //     }
-
-    //     // console.log(count);
-
-    // }, 10);
     var going1 = window.setInterval(function() {
         var count1 = $(targetBlock).data('count');
 
@@ -87,6 +41,7 @@ $(document).ready(function() {
     //stop animation when you hit spacebar!
     window.addEventListener("keydown", keysPressed, false);
     window.addEventListener("keyup", keysReleased, false);
+    var targetBlock;
 
     var keys = [];
     var keyPressCount = 0;
@@ -107,25 +62,15 @@ $(document).ready(function() {
             keyPressCount++;
             targetBlock = '.tester' + keyPressCount
             console.log(targetBlock)
-                // animate the div
-                // console.log(going)
-            // window.clearInterval(going1);
+
 
 
             $(targetBlock).css('visibility', 'visible');
-            // e.stopPropagation();
-            // window.clearInterval(going1);
 
 
-            // prevent default browser behavior
-
-
-
-            //animate div again
-
-        }// if (targetBlock !== $('.stacker').css(left)){
+        } // if (targetBlock !== $('.stacker').css(left)){
         // alert('blah')
-         //}
+        //}
 
         e.preventDefault();
     }
